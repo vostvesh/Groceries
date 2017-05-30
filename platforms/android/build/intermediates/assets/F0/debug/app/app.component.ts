1081409a7476e5ec8acc1d05d1191615805485a9
vostvesh <vostvesh@gmail.com>
2017-05-30 12:01:41 +0300

    Init

---
+++ platforms/android/build/intermediates/assets/F0/debug/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <TextField hint="Email Address" keyboardType="email"></TextField>
+  `
+})
+export class AppComponent {}
---
+++ platforms/android/src/main/assets/app/pages/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list',
+  templateUrl: 'pages/list/list.html',
+  styleUrls: ['pages/list/list-common.css', 'pages/list/list.css']
+})
+export class ListComponent {
+
+}
