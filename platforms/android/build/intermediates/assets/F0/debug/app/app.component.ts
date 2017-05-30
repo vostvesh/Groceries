da469cc65aeca8066dd3eacef72670e440ae0fef
vostvesh <vostvesh@gmail.com>
2017-05-30 12:26:55 +0300

    Init

--- platforms/android/build/intermediates/assets/F0/debug/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "my-app",
-  template: `
-    <TextField hint="Email Address" keyboardType="email"></TextField>
-  `
-})
-export class AppComponent {}
